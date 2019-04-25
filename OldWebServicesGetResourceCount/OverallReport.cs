52f9c4d4643c401696e17d1dcc8ac7bb728cd680
jamesmccaff <jamesmccaffrey2@googlemail.com>
2019-04-25 13:49:57 +0100

    Initial Classes added.

---
+++ OldWebServicesGetResourceCount/OverallReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectorAutomation
+{
+   public class OverallReport
+  {
+        public string ReprortingUnit { get; set; }
+        public DateTime PrintDateTime { get; set; }
+        public string LocalInstance { get; set; }
+        public Decimal Currency { get; set; }
+        public string StartWeek { get; set; }
+        public string EndWeek { get; set; }
+        List<Person> People { get; set; }
+    }
+}
---
+++ OldWebServicesGetResourceCount/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectorAutomation
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public List<Job> Jobs { get; set; }
+    }
+}
